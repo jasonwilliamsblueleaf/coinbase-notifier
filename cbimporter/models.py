50b187cd96cee54dda7abcc473cf851a4d8ca50c
Jason Williams <20069998+jasonwilliamsblueleaf@users.noreply.github.com>
2019-05-25 16:18:52 -0400

    initial tables for cbimporter

---
+++ cbimporter/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+import cbpro
+import os
+
+auth_client = cbpro.AuthenticatedClient(
+	os.environ['CB_KEY'],
+	os.environ['B64SECRET'],
+	os.environ['PASSPHRASE']
+)
+
+class Portfolio(models.Model):
+	updated_at = models.DateTimeField(auto_now=True)
+	total_value = models.DecimalField(max_digits=8, decimal_places=2)
+	high_date = models.DateTimeField()
+	high_value = models.DecimalField(max_digits=8, decimal_places=2)
+
+class Account(models.Model):
+	portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+	cb_id = models.CharField(max_length=36, unique=True)
+	currency = models.CharField(max_length=4)
+	quantity = models.DecimalField(max_digits=25, decimal_places=16)
+	available = models.DecimalField(max_digits=25, decimal_places=16)
+	hold = models.DecimalField(max_digits=25, decimal_places=16)
+	price = models.DecimalField(max_digits=16, decimal_places=8)
+	value = models.DecimalField(max_digits=16, decimal_places=8)
