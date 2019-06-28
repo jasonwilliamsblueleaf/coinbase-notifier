305e7852b3f78ca644f3cdec2bae7a1ebe0a2b07
Jason Williams <20069998+jasonwilliamsblueleaf@users.noreply.github.com>
2019-06-27 20:36:36 -0400

    2nd attempt at avoiding sms spam filter

--- notifier/management/commands/high_sms.py
+++ notifier/management/commands/high_sms.py
@@ -9,9 +9,9 @@ class Command(BaseCommand):
 		cb_portfolio.update_all()
 		if cb_portfolio.is_high():
 			cb_portfolio.update_high()
-			sms_text = 'New High!\nTotal Value: $%s'%(round(cb_portfolio.total_value,2))
+			sms_text = 'New High!\nTotal Value: %s'%(round(cb_portfolio.total_value,2))
 			for account in cb_portfolio.account_set.all():
-				sms_text += '\n%s price: %s'%(account.currency, round(account.price, 2))
+				sms_text += '\n%s: %s'%(account.currency, round(account.price, 2))
 			requests.post(TILL_URL, json={
 			    "phone": os.environ.get("CELL_PHONE"),
 			    "text" : sms_text
